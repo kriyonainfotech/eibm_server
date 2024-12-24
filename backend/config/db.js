e3b5ea85305a9c403e213837cb2b33eb3b926537
prarthana-v <p.d.vaghani1729@gmail.com>
2024-12-24 16:29:38 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const ConnectDb = async () => {
   try {
     const db = await mongoose.connect(
-      "mongodb+srv://eibm:eibm@cluster0.zbfjd.mongodb.net//EIBM_APP"
+      "mongodb+srv://eibm:eibm@cluster0.zbfjd.mongodb.net/eibm"
     );
     console.log(`Mongodb Connected : --  ${db.connection.host}`);
   } catch (error) {
