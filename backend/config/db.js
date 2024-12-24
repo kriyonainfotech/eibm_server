c606ff6f951b4d91d666831e0c126b70f2099166
prarthana-v <p.d.vaghani1729@gmail.com>
2024-12-24 16:32:59 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 const ConnectDb = async () => {
   try {
     const db = await mongoose.connect(
-      "mongodb+srv://eibm:eibm@cluster0.zbfjd.mongodb.net/eibm"
+      "mongodb+srv://eibm:eibm@cluster0.zbfjd.mongodb.net/eibm",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        socketTimeoutMS: 45000, // Increase timeout
+        connectTimeoutMS: 30000, // Increase connection timeout
+      }
     );
     console.log(`Mongodb Connected : --  ${db.connection.host}`);
   } catch (error) {
