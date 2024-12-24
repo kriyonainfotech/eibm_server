49626077952407ed0a5475e45c50e15c5af1991f
prarthana-v <p.d.vaghani1729@gmail.com>
2024-12-24 16:20:40 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
-const mongoose  = require('mongoose')
-const ConnectDb = async() => {
-    try {
-        const db = await mongoose.connect('mongodb+srv://sujaltank13:sujaltank13@cluster0.7duu8xj.mongodb.net/EIBM_APP')
-        console.log(`Mongodb Connected : --  ${db.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
-module.exports = ConnectDb
+const mongoose = require("mongoose");
+const ConnectDb = async () => {
+  try {
+    const db = await mongoose.connect(
+      "mongodb+srv://eibm:eibm@cluster0.zbfjd.mongodb.net//EIBM_APP"
+    );
+    console.log(`Mongodb Connected : --  ${db.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = ConnectDb;
